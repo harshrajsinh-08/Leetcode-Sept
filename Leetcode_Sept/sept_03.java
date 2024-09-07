123b8c19d8bb057c24086ee4d953b1f51be0775f
Harshraj <harshrajsinhz1684@gmail.com>
2024-09-07 11:39:24 +0530

    robot obstacles

--- Leetcode_Sept/sept_03.java
+++ Leetcode_Sept/sept_03.java
@@ -1,23 +1,39 @@
+import java.util.*;
 class sept_03 {
-    public int Lucky(String s, int k) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            sb.append(c - 'a'+1);
+    public int robotSim(int[] commands, int[][] obstacles) {
+        int[] dirs = {0, 1, 0, -1, 0};
+        Set<Integer> s = new HashSet<>(obstacles.length);
+        for (var e : obstacles) {
+            s.add(f(e[0], e[1]));
         }
-        s = sb.toString();
-        while (k-- > 0) {
-            int t = 0;
-            for (char c : s.toCharArray()) {
-                t += c - '0';
+        int ans = 0, k = 0;
+        int x = 0, y = 0;
+        for (int c : commands) {
+            if (c == -2) {
+                k = (k + 3) % 4;
+            } else if (c == -1) {
+                k = (k + 1) % 4;
+            } else {
+                while (c-- > 0) {
+                    int nx = x + dirs[k], ny = y + dirs[k + 1];
+                    if (s.contains(f(nx, ny))) {
+                        break;
+                    }
+                    x = nx;
+                    y = ny;
+                    ans = Math.max(ans, x * x + y * y);
+                }
             }
-            s = String.valueOf(t);
         }
-        return Integer.parseInt(s);
+        return ans;
+    }
+    public int f(int x, int y) {
+        return x * 60010 + y;
     }
     public static void main(String[] args) {
-        String s = "zbax";
-        int k = 2;
+        int[] commands = {4, -1, 3};
+        int[][] obstacles = {{2, 4}};
         sept_03 obj = new sept_03();
-        System.out.println(obj.Lucky(s, k));
+        System.out.println(obj.robotSim(commands, obstacles));
     }
 }
